b3e651309e951b49acac40434e618f08bd7b553e
Gaetano <147176705+GaetanoRomeo22@users.noreply.github.com>
2024-11-06 15:06:06 +0100

    Add files via upload

--- Romeo_Temporal_Fusion_Transformer.py
+++ Romeo_Temporal_Fusion_Transformer.py
@@ -1,3 +1,12 @@
+"""-------------------------------------------------------------------------------------------------
+Il seguente codice ha come obiettivo quello di cercare di predire, a partire da una serie di
+osservazioni dei Campi Flegrei di Napoli ad opera di varie stazioni, il valore di una delle tre
+coordinate nel sistema ENU (East, North, Up).
+Le osservazioni sono scandite ad intervalli orari e il modello di apprendimento utilizzato
+è il Temporal Fusion Transformer.
+Autore: Gaetano Romeo
+-------------------------------------------------------------------------------------------------"""
+
 import csv
 import os
 from math import modf
@@ -18,6 +27,14 @@ from lightning.pytorch.tuner import Tuner
 # Logger per TensorBoard (richiede TensorBoard tra gli import)
 # logger = TensorBoardLogger("lightning_logs")
 
+"""-------------------------------------------------------------------------------------------------
+Per una più comoda gestione del dataset, ho convertito tutti i file .rtl in un unico file .csv.
+Gli unici campi memorizzati sono l'ID, l'ora, la data e il valore da predire (East, North o Height).
+Siccome è stato notato che il valore intero per le osservazioni è costante, per il campo target
+è stata estratta solo la parte decimale.
+Avendo a disposizione dati compresi tra i valori 0.1 e 0.9, è stato ritenuto opportuno non adottare
+alcuna normalizzazione.
+-------------------------------------------------------------------------------------------------"""
 def rtl_to_csv(folder_path, output_file):  # Converto il contenuto dei file rtl in un unico csv
     with open(output_file, mode='w', newline='') as csv_file:  # Apro il file di output in scrittura
         csv_writer = csv.writer(csv_file, delimiter=',')  # csv_writer per scrivere sul file
@@ -30,8 +47,8 @@ def rtl_to_csv(folder_path, output_file):  # Converto il contenuto dei file rtl 
                         file_data = row.split(',')  # Divido il contenuto del rigo per la virgola
                         time_str = file_data[1].strip()  # Estraggo l'orario dalla colonna corretta e rimuovo spazi
                         time = f"{time_str[:2]}:{time_str[2:4]}:{time_str[4:6]}" # Converto il tempo in formato HH:MM:SS
-                        date = pd.to_datetime(file_data[2], format='%m%d%y')  # Estraggo la data
-                        if date.month == 3 and date.year == 2022 or date.month == 1 and date.year == 2023:  # Salta marzo 2022 e gennaio 2023
+                        date = pd.to_datetime(file_data[2], format='%m%d%y')  # Estraggo la data in formato MM-DD-YYYY
+                        if date.month == 3 and date.year == 2022 or date.month == 1 and date.year == 2023:  # Salta marzo 2022 e gennaio 2023 perchè ci sono pochi dati
                             continue
                         target = modf(float(file_data[10]))[0]  # Estraggo il valore di Grid Northing (parte decimale)
                         csv_writer.writerow(['$GPLLQ', time, date.strftime('%d-%m-%Y'), target]) # Scrivo i dati su file
@@ -40,27 +57,27 @@ def rtl_to_csv(folder_path, output_file):  # Converto il contenuto dei file rtl 
 
 if __name__ == '__main__':
     # Caricamento e settaggio del dataset
-    rtl_to_csv("SOLO-LICO", "Dataset.csv") # Converto i file rtl in csv
-    dataset = pd.read_csv("Dataset.csv")  # Creo un dataframe con pandas
+    rtl_to_csv("RITE-LICO", "RITE-LICO_Dataset.csv") # Converto i file rtl in csv
+    dataset = pd.read_csv("RITE-LICO_Dataset.csv")  # Creo un dataframe con pandas
     dataset = dataset.dropna()  # Rimuovo eventuali record incompleti
     dataset['date'] = pd.to_datetime(dataset['date'], format='%d-%m-%Y') # Converto la data nel formato corretto
     dataset['month'] = dataset.date.dt.month.astype(str) # Aggiungo l'informazione relativa al mese
     dataset['year'] = dataset.date.dt.year.astype(str) # Aggiungo l'informazione relativa all'anno
-    dataset['time_idx'] = dataset.groupby(['month']).cumcount()  # Aggiungo una colonna per il time index per il TFT
+    dataset['time_idx'] = dataset.groupby(['month']).cumcount()  # Aggiungo una colonna per il time index per il TFT raggruppando per il mese
     print(dataset.head())  # Stampo alcuni dati
-    train_cnt = int(len(dataset) * .8)  # 80% train e 20% test
+    train_cnt = int(len(dataset) * .8)  # Divido il dataset in 80% train e 20% test
     train = dataset.iloc[:train_cnt] # Dati di train
     test = dataset.iloc[train_cnt:] # Dati di test
     max_prediction_length = 24 # Numero di osservazioni da predire
     max_encoder_length = 168 # Numero di osservazioni da analizzare per le predizioni
     batch_size = 64
-    epochs = 50 # Numero di epoche
+    epochs = 10 # Numero di epoche
 
     training = TimeSeriesDataSet( # Converto il dataset nel formato richiesto dal TFT
         train, # Dati di addestramento
         time_idx='time_idx', # Indice temporale per le serie temporali
         target='height', # Valore da predire
-        group_ids=['month'], # Campo utilizzato per identificare univocamente le serie temporali (da modificare!!!!)
+        group_ids=['month'], # Campo utilizzato per identificare univocamente le serie temporali
         min_encoder_length=max_encoder_length // 2, # Numero minimo di osservazioni da analizzare per le predizioni
         max_encoder_length=max_encoder_length, # Numero di osservazioni da analizzare per le predizioni
         min_prediction_length=1, # Numero minimo di osservazioni da predire
@@ -83,6 +100,12 @@ if __name__ == '__main__':
     train_dataloader = training.to_dataloader(train=True, batch_size=batch_size, num_workers=8, persistent_workers=True)
     val_dataloader = validation.to_dataloader(train=False, batch_size=batch_size * 10, num_workers=8, persistent_workers=True)
 
+    """-------------------------------------------------------------------------------------------------
+    Le seguenti istruzioni hanno l'obiettivo di analizzare il dataset e il modello da utilizzare
+    per determinare il valore di learning rate ottimale da utilizzare per la fase di training.
+    Una volta trovato il valore, lo si memorizza ed è possibile visualizzarne il grafico.
+    Il valore è cercato nell'intervallo [0.01, 0.0001] consigliato nel paper di riferimento del TFT.
+    -------------------------------------------------------------------------------------------------"""
     # Fase di ricerca del miglior learning rate con Tuner
     pl.seed_everything(42)
     trainer = pl.Trainer(
@@ -112,6 +135,18 @@ if __name__ == '__main__':
     # fig = res.plot(show=True, suggest=True)
     # fig.show()
 
+    """-------------------------------------------------------------------------------------------------
+    La seguente funzione ha l'obiettivo di trovare i pesi ottimali di addestramento per il modello.
+    Essa effettua un numero specificato dall'utente di tentativi di addestramento con l'obiettivo di
+    minimizzare il valore della validation loss.
+    L'intervallo di valori da provare per ogni peso è consigliato nel paper di riferimento del TFT.
+    E' presente un monitor che ferma l'addestramento nel caso in cui si nota che il modello in più
+    iterazioni consecutive non migliora le proprie prestazioni.
+    L'utilizzo di Optuna permette di generare un file .db dal quale è possibile osservare informazioni
+    circa l'addestramento mediante la dashboard da terminale.
+    Una volta trovati i migliori parametri di addestramento, si salva il miglior modello trovato
+    per poi ricaricarlo una volta che si vuole iniziare la fase di validazione.
+    -------------------------------------------------------------------------------------------------"""
     def objective(trial): # Funzione per trovare i migliori pesi di addestramento
 
         # Dominio dei valori da provare per ogni peso
@@ -199,6 +234,13 @@ if __name__ == '__main__':
     best_model_path = trainer.checkpoint_callback.best_model_path
     best_tft = TemporalFusionTransformer.load_from_checkpoint(best_model_path)
 
+    """-------------------------------------------------------------------------------------------------
+    La seguente parte di codice mette in evidenza la fase di validazione del modello.
+    Come prima cosa si ricavano le predizioni del modello e le si visualizzano graficamente.
+    Successivamente si visualizzano le interpretazioni del modello in termini di attenzione e importanza
+    di ogni parametro del dataset.
+    Infine è visualizzato un grafico che mette a confronto i valori predetti e quelli effettivi.
+    -------------------------------------------------------------------------------------------------"""
     # Stampa delle predizioni
     predictions = best_tft.predict(val_dataloader, mode="raw", return_x=True, return_y=True)
     best_tft.plot_prediction(predictions.x, predictions.output, idx=0, add_loss_to_title=SMAPE(quantiles=best_tft.loss.quantiles))
